f1cc0a199b342b9a5b710523d07a3e253f01410a
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-05-29 18:07:05 +0530

    added note points on Array list

--- src/com/tcs/collection/ArrayListExample.java
+++ src/com/tcs/collection/ArrayListExample.java
@@ -1,7 +1,13 @@
 package com.tcs.collection;
 
 import java.util.*;
-
+/**
+ * 
+ * Array List Maintains the insertion order.
+ * by default the size of Array list is 10.
+ * Array list is Not thread Safe.  
+ *
+ */
 public class ArrayListExample {
 	public static void main(String[] args) {
 		Collection<String> listOfNumbers=new ArrayList<>();
@@ -26,13 +32,30 @@ public class ArrayListExample {
 		System.out.println("List of values after adding the value at certain index: -\n"+listOfNums+"\n");
 		
 		listOfNums.addAll(listOfNumbers);//add all
-		System.out.println("after adding another list: -"+listOfNums);
+		System.out.println("after adding another list: -\n"+listOfNums+"\n");
 		
-//		Note:- remove all values which are matched from other list of values
+		System.out.println("is their all other list of values:- \n"+listOfNums.containsAll(listOfNumbers)+"\n");//containsAll
+		
+//		Note:- remove all values which are matched from other list of values.
 		listOfNums.removeAll(listOfNumbers);//remove all 
-		System.out.println("after Removing all the values from Other list: -"+listOfNums);
+		System.out.println("after Removing all the values from Other list: -\n"+listOfNums+"\n");
+		
+		listOfNums.addAll(0, listOfNumbers);//add all at certain index.
+		System.out.println("added list of numbers at 0 index: -\n"+listOfNums+"\n");
+		
+		System.out.println("Iterrating each value by using for each: -");
+		for(String obj : listOfNums) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n"+"Iterrating each value by using for each method: -");
+		listOfNums.forEach(value -> System.out.println(value));//using lambda expression.
+		
+		System.out.println("\n"+"Iterrating each value by using for each method: -");
+		listOfNums.forEach(System.out::println);//by using method references.
+		System.out.println();
 		
 		listOfNums.clear();//clear
-		System.out.println("list of values after clear: -"+listOfNums);
+		System.out.println("list of values after clear: -\n"+listOfNums);
 	}
 }
